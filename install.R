392fba4b580d4f2201af17bd41b63eb4ec3f3bdd
jcaldous <42592431+jcaldous@users.noreply.github.com>
2020-11-05 11:47:28 -0500

    Create install.R

---
+++ install.R
@@ -0,0 +1,7 @@
+devtools::install_github("thomasp85/patchwork")
+install.packages("palmerpenguins", repos="https://cran.rstudio.com")
+install.packages("ggrepel", repos="https://cran.rstudio.com")
+setRepositories(ind=1:2)
+devtools::install_github("GuangchuangYu/ggimage")
+install.packages(c("devtools", "ggimage", "png", "RCurl", "TeachingDemos", "tidyverse","ggimage","dplyr"))
+
