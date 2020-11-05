7abccf419b471e8c1567c04ea596c2cde09466e1
jcaldous <42592431+jcaldous@users.noreply.github.com>
2020-11-05 12:01:09 -0500

    Update install.R

--- install.R
+++ install.R
@@ -3,5 +3,3 @@ install.packages("palmerpenguins", repos="https://cran.rstudio.com")
 install.packages("ggrepel", repos="https://cran.rstudio.com")
 setRepositories(ind=1:2)
 devtools::install_github("GuangchuangYu/ggimage")
-install.packages(c("devtools", "ggimage", "png", "RCurl", "TeachingDemos", "tidyverse","ggimage","dplyr"))
-
