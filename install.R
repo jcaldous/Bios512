77b7707adfce9b497a4d8a3b0042b937343224f7
jcaldous <42592431+jcaldous@users.noreply.github.com>
2020-11-05 14:17:25 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,6 +1,5 @@
-brew install imagemagick@6
 devtools::install_github("thomasp85/patchwork")
 #devtools::install_github("ropensci/magick")
+#install.packages("magick", repos="https://cran.rstudio.com")
 install.packages("ggrepel", repos="https://cran.rstudio.com")
-install.packages("magick", repos="https://cran.rstudio.com")
 devtools::install_github("GuangchuangYu/ggimage")
