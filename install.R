9edc994463853eae3ffa600c883b6539507fbb19
jcaldous <42592431+jcaldous@users.noreply.github.com>
2020-11-12 12:45:31 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 devtools::install_github("thomasp85/patchwork")
-#devtools::install_github("ropensci/magick")
-#install.packages("magick", repos="https://cran.rstudio.com")
+devtools::install_github("ropensci/magick")
+devtools::install_github("GuangchuangYu/ggimage")
 install.packages("ggrepel", repos="https://cran.rstudio.com")
 #devtools::install_github("GuangchuangYu/ggimage")
